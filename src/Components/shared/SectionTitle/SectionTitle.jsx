1c0e6ce0f130322b6275899d12516b253082bf86
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-02-18 01:41:38 +0530

    Section title size issue fix

--- src/Components/shared/SectionTitle/SectionTitle.jsx
+++ src/Components/shared/SectionTitle/SectionTitle.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 const SectionTitle = ({ heading }) => {
   return (
-    <div className="lg:w-5/12 w-1/2 mx-auto text-center mt-5 mb-5">
-      <h2 className="uppercase  border-y-2 text-teal-500 text-3xl md:text-4xl py-4">
+    <div className="lg:w-5/12 w-10/12 mx-auto text-center mt-5 mb-5">
+      <h2 className="uppercase  border-y-2 text-teal-500 text-2xl md:text-4xl py-4">
         {heading}
       </h2>
     </div>
